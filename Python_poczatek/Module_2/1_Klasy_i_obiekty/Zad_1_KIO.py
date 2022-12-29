61c742ac5e819597c789605dfc232449e2c97f1b
Daniel Iwanowski <daniel_iwanowski@o2.pl>
2022-12-29 14:22:16 +0100

    Klasy i obiekty 2

--- Python_poczatek/Module_2/1_Klasy_i_obiekty/Zad_1_KIO.py
+++ Python_poczatek/Module_2/1_Klasy_i_obiekty/Zad_1_KIO.py
@@ -10,3 +10,16 @@ class Apple:
 class Potato:
     pass
 
+jablko = Apple()
+ziemniak = Potato()
+
+print(type(jablko))
+print(type(ziemniak))
+products_dict = {
+    "butter": Products(),
+    "soap": Products(),
+    "papier": Products()
+}
+products_list = [Order(), Order(), Order(), Order(), Order()]
+print(products_list)
+print(products_dict)
