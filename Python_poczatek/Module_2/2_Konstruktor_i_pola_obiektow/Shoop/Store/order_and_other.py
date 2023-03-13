4338f78aaaf3f193424708431d8d9f0c74ee859c
Daniel Iwanowski <daniel_iwanowski@o2.pl>
2023-03-13 13:27:23 +0100

    Klasy i obiekty 2

--- Python_poczatek/Module_2/2_Konstruktor_i_pola_obiektow/Shoop/Store/order_and_other.py
+++ Python_poczatek/Module_2/2_Konstruktor_i_pola_obiektow/Shoop/Store/order_and_other.py
@@ -3,12 +3,16 @@ from Store.order_element import OrderElement
 import random
 class Order:
 
+    MAX_ORDER_ELEMENT_LIMIT = 20
     def __init__(self, first_name, second_name, positions_list=None):
         self.second_name = second_name
         self.first_name = first_name
         if positions_list is None:
             positions_list = []
-        self._positions_list = positions_list
+        if len(positions_list) > Order.MAX_ORDER_ELEMENT_LIMIT:
+            self._positions_list = positions_list[:Order.MAX_ORDER_ELEMENT_LIMIT]
+        else:
+            self._positions_list = positions_list
         self.total_price = self._total_price_elements()
 
     def _total_price_elements(self):
@@ -18,8 +22,11 @@ class Order:
         return total_price
 
     def add_new_product(self, product, quantity):
-        self._positions_list.append(OrderElement(product, quantity))
-        self.total_price = self._total_price_elements()
+        if len(self._positions_list) >= Order.MAX_ORDER_ELEMENT_LIMIT:
+            print(f"Brak możliwości dodania produktu, przekroczono limit w zamówieniu")
+        else:
+            self._positions_list.append(OrderElement(product, quantity))
+            self.total_price = self._total_price_elements()
 
     def __str__(self):
         information_result = f"\n Imię: {self.first_name}| Nazwisko: {self.second_name} \n Wartość zamówienia {self.total_price}"
@@ -44,20 +51,20 @@ class Order:
         return True
 
 
+    @classmethod
+    def generator_order(cls):
+        number_order = random.randint(1, 40)
+        position_list = []
+        for eny_number in range(number_order):
+            name_product = f"Produkt {eny_number}"
+            cathegory = "Inne"
+            product_number = random.randint(1, 10)
+            new_product = Product(name=name_product, cathegory=cathegory, price=product_number)
+            quantity_number = random.randint(1, 5)
+            position_list.append(OrderElement(product=new_product, quantity=quantity_number))
 
-def generator_order():
-    number_order = random.randint(1, 20)
-    position_list = []
-    for eny_number in range(number_order):
-        name_product = f"Produkt {eny_number}"
-        cathegory = "Inne"
-        product_number = random.randint(1, 10)
-        new_product = Product(name=name_product, cathegory=cathegory, price=product_number)
-        quantity_number = random.randint(1, 5)
-        position_list.append(OrderElement(product=new_product, quantity=quantity_number))
+        new_order = Order(first_name="Daniel", second_name="Iwanowski", positions_list=position_list)
 
-    new_order = Order(first_name="Daniel", second_name="Iwanowski", positions_list=position_list)
-
-    return new_order
+        return new_order
 
 
--- Python_poczatek/Module_2/2_Konstruktor_i_pola_obiektow/Shoop/main_shoop.py
+++ Python_poczatek/Module_2/2_Konstruktor_i_pola_obiektow/Shoop/main_shoop.py
@@ -1,16 +1,16 @@
 from Store.klasa_Apple import Apple
 from Store.Klasa_Patato import Potato
-from Store.order_and_other import Order, generator_order
+from Store.order_and_other import Order
 from Store.product_and_other import Product
 from Store.order_element import OrderElement
 
 
 def run_example():
 
-    first_order = generator_order()
+    first_order = Order.generator_order()
     print(first_order)
     print(f"Liczba pozycji w zamówieniu: {len(first_order)}\n\n")
-    second_order = generator_order()
+    second_order = Order.generator_order()
     print(second_order)
     print(f"Liczba pozycji w zamówieniu: {len(second_order)}\n\n")
 
@@ -20,19 +20,19 @@ def run_example():
     old_potato = Potato(kind="Old", size="H", price=3.69, weight_potato=10)
     print(repr(old_potato))
 
-    other_salat = Product(name="Green salat", cathegory="Vegetable", price=8.99)
-    new_other_order = Product(name="Green salat", cathegory="Vegetable", price=8.99)
-    first_element = OrderElement(product="Marchewki", quantity=20)
-    second_element = OrderElement(product="Marchewki", quantity=20)
+    # other_salat = Product(name="Green salat", cathegory="Vegetable", price=8.99)
+    # new_other_order = Product(name="Green salat", cathegory="Vegetable", price=8.99)
+    # first_element = OrderElement(product="Marchewki", quantity=20)
+    # second_element = OrderElement(product="Marchewki", quantity=20)
+    # print("\n\n")
+    #
+    # # print(first_element == second_element)
+    # # print(first_order == second_order)
+    # # print(other_salat == new_other_order)
+
     print("\n\n")
 
-    print(first_element == second_element)
-    print(first_order == second_order)
-    print(other_salat == new_other_order)
-
-    print("\n\n")
-
-    third_order = generator_order()
+    third_order = Order.generator_order()
     print(third_order)
     new_product = Product(name="Maka", cathegory="Food", price=10.99)
     third_order.add_new_product(product=new_product, quantity=6)
