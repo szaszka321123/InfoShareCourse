6036996a8a2202999abca781db75cbeaff2bcaaa
Daniel Iwanowski <daniel_iwanowski@o2.pl>
2023-03-17 16:17:59 +0100

    Klasy i obiekty 2

--- Python_poczatek/Module_2/3_Unpacking_operators/main_unp.py
+++ Python_poczatek/Module_2/3_Unpacking_operators/main_unp.py
@@ -2,11 +2,30 @@ from exercises.exerc_2 import named_arguments
 
 def run_example():
 
-    frist_list = [1, 2, 3, 4, 5, 6]
-    second_list = [7, 8, 9, 10, 11]
+    first_dict = {
+        "Imię": "Daniel",
+        "Nazwisko": "Kowslaski",
+        "Wiek": "26",
+        "Wzrost": "187"
+    }
 
-    new_list = [*frist_list, *second_list]
-    print(new_list)
+    second_dict = {
+        "Imię": "Agata",
+        "Nazwisko": "Kowslaski",
+        "Wiek": "27",
+        "Wzrost": "172"
+    }
+
+    third_dict = {
+        "Imię": "Other",
+        "Nazwisko": "Kowslaski",
+        "Wiek": "25",
+        "Wzrost": "150"
+    }
+
+    new_dict = {**first_dict, **second_dict, **third_dict}
+    print(new_dict)
+    named_arguments(**new_dict)
 
 if __name__ == "__main__":
     run_example()
