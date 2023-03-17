dcda2379755231a5d01b2fd35f6e6515508b199b
Daniel Iwanowski <daniel_iwanowski@o2.pl>
2023-03-17 15:50:49 +0100

    Klasy i obiekty 2

--- Python_poczatek/Module_2/3_Unpacking_operators/main_unp.py
+++ Python_poczatek/Module_2/3_Unpacking_operators/main_unp.py
@@ -2,14 +2,11 @@ from exercises.exerc_2 import named_arguments
 
 def run_example():
 
-    new = named_arguments(
-        Imię = "Daniel",
-        Nazwisko = "Iwanowski",
-        Wiek = 26,
-        Wzrost = 187
-    )
-    print(new)
+    frist_list = [1, 2, 3, 4, 5, 6]
+    second_list = [7, 8, 9, 10, 11]
 
+    new_list = [*frist_list, *second_list]
+    print(new_list)
 
 if __name__ == "__main__":
     run_example()
