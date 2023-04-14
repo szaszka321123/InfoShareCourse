831ee9a286b018f3941c0ed5d5dfad475a88b6af
Daniel Iwanowski <daniel_iwanowski@o2.pl>
2023-04-14 12:00:14 +0200

    Klasy i obiekty 2

--- Python_poczatek/Module_4/Wbudowane_struktury_danych/Zadania/int_oraz_float.py
+++ Python_poczatek/Module_4/Wbudowane_struktury_danych/Zadania/int_oraz_float.py
@@ -1,7 +1,36 @@
 import random
-
+import math
 def draw_float_type():
     return random.uniform(-20, 20)
 
 def draw_int_type():
     return random.randint(1, 10)
+
+
+def int_float_run_example():
+
+    first_float = draw_float_type()
+    second_float = draw_float_type()
+    third_float = draw_float_type()
+    fourth_float = draw_float_type()
+    fifth_float = draw_float_type()
+    sixth_float = draw_float_type()
+
+    first_int = draw_int_type()
+    second_int = draw_int_type()
+    third_int = draw_int_type()
+
+    print(first_float)
+    print(second_float)
+    print(third_float)
+    print(first_int)
+    print(second_int)
+    print(third_int)
+
+    print(round(first_float))
+    print(math.ceil(second_float))
+    print(math.floor(third_float))
+
+    print(fourth_float ** first_int)
+    print(pow(fifth_float, second_int))
+    print(math.pow(sixth_float, third_int))
--- Python_poczatek/Module_4/Wbudowane_struktury_danych/main_struktury_danych.py
+++ Python_poczatek/Module_4/Wbudowane_struktury_danych/main_struktury_danych.py
@@ -3,31 +3,8 @@ import math
 
 
 def run_example():
-    first_float = int_oraz_float.draw_float_type()
-    second_float = int_oraz_float.draw_float_type()
-    third_float = int_oraz_float.draw_float_type()
-    fourth_float = int_oraz_float.draw_float_type()
-    fifth_float = int_oraz_float.draw_float_type()
-    sixth_float = int_oraz_float.draw_float_type()
 
-    first_int = int_oraz_float.draw_int_type()
-    second_int = int_oraz_float.draw_int_type()
-    third_int = int_oraz_float.draw_int_type()
-
-    print(first_float)
-    print(second_float)
-    print(third_float)
-    print(first_int)
-    print(second_int)
-    print(third_int)
-
-    print(round(first_float))
-    print(math.ceil(second_float))
-    print(math.floor(third_float))
-
-    print(fourth_float ** first_int)
-    print(pow(fifth_float, second_int))
-    print(math.pow(sixth_float, third_int))
+    print(int_oraz_float.int_float_run_example())
 
 
 
